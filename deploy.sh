711eaacf87df800b839db9e9469350b39d7ed022
Yaniv Peretz <yaniv.peretz@gm.com>
2022-09-18 17:03:15 +0300

    update client

--- deploy.sh
+++ deploy.sh
@@ -16,13 +16,12 @@ cd dist
 
 # git init
 # git checkout -b main
-# git add -A
-# git commit -m 'deploy'
+git commit -am 'update client'
 
 # if you are deploying to https://<USERNAME>.github.io
 # git push -f git@github.com:<USERNAME>/<USERNAME>.github.io.git main
 
 # if you are deploying to https://<USERNAME>.github.io/<REPO>
-git push -f git@github.com:Yanipo5/Todo-Vue-3.git main:gh-pages
+git push git@github.com:Yanipo5/Todo-Vue-3.git main:gh-pages
 
 cd -
