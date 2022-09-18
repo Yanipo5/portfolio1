50b701415a2824fd3bcfb6b1779d183737fc3686
Yaniv Peretz <yaniv.peretz@gm.com>
2022-09-18 15:59:17 +0300

    deploy

--- vite.config.ts
+++ vite.config.ts
@@ -1,24 +1,27 @@
-import { fileURLToPath, URL } from 'node:url';
+import { fileURLToPath, URL } from "node:url";
 
-import { defineConfig } from 'vite';
-import vue from '@vitejs/plugin-vue';
+import { defineConfig } from "vite";
+import vue from "@vitejs/plugin-vue";
 
 // https://vitejs.dev/config/
 export default defineConfig({
-   plugins: [vue()],
-   resolve: {
-      alias: {
-         '@': fileURLToPath(new URL('./src', import.meta.url)),
+  plugins: [vue()],
+  resolve: {
+    alias: {
+      "@": fileURLToPath(new URL("./src", import.meta.url)),
+    },
+  },
+  base: "/portfolio1",
+  server: {
+    port: 3001,
+    proxy: {
+      "/trpc": {
+        target: "http://localhost:8080",
+        changeOrigin: true,
+        secure: false,
       },
-   },
-   server: {
-      port: 3001,
-      proxy: {
-         '/trpc': {
-            target: 'http://localhost:8080',
-            changeOrigin: true,
-            secure: false,
-         },
-      },
-   },
+    },
+  },
 });
+
+// Deploy to Github: https://vitejs.dev/guide/static-deploy.html#github-pages
