73af9acffcae42f7a7e92675a97a42ff53ebef5d
Yaniv Peretz <yaniv.peretz@gm.com>
2022-09-18 16:21:29 +0300

    comments 2

--- deploy.sh
+++ deploy.sh
@@ -14,21 +14,15 @@ cd dist
 # if you are deploying to a custom domain
 # echo 'www.example.com' > CNAME
 
-echo "1"
 git init
-# echo "2"
 # git checkout -b main
-echo "3"
 git add -A
-echo "4"
 git commit -m 'deploy'
-echo "5"
 
 # if you are deploying to https://<USERNAME>.github.io
 # git push -f git@github.com:<USERNAME>/<USERNAME>.github.io.git main
 
 # if you are deploying to https://<USERNAME>.github.io/<REPO>
 git push -f git@github.com:Yanipo5/portfolio1.git main:gh-pages
-echo "6"
 
 cd -
