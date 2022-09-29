a4b2da19375ae2b3348ce21a1203154b49754748
Yaniv Peretz <yip528@gmail.com>
2022-09-29 17:58:43 +0300

    animations

--- src/App.vue
+++ src/App.vue
@@ -1,8 +1,8 @@
 <script setup lang="ts">
-import TodoItem from "./components/TodoItem.vue";
-import AppController from "./components/AppController.vue";
-import CompletedTodos from "./components/CompletedTodos.vue";
-import useTodosStore from "@/stores/todos";
+import TodoItem from './components/TodoItem.vue';
+import AppController from './components/AppController.vue';
+import CompletedTodos from './components/CompletedTodos.vue';
+import useTodosStore from '@/stores/todos';
 
 const store = useTodosStore();
 </script>
@@ -10,7 +10,9 @@ const store = useTodosStore();
 <template>
   <div class="app">
     <AppController class="app-controller" />
-    <TodoItem v-for="todo in store.unCompletedTodos" :todo="todo" class="todo-item" />
+    <TransitionGroup name="list">
+      <TodoItem v-for="todo in store.unCompletedTodos" :todo="todo" class="todo-item" :key="todo.id" />
+    </TransitionGroup>
     <CompletedTodos />
   </div>
 </template>
@@ -21,5 +23,15 @@ const store = useTodosStore();
 }
 .todo-item {
   margin-bottom: 0.5vh;
+}
+
+.list-enter-active,
+.list-leave-active {
+  transition: all 0.5s ease-in;
+}
+.list-enter-from,
+.list-leave-to {
+  opacity: 0;
+  transform: translateY(-15px);
 }
 </style>
--- src/components/CompletedTodos.vue
+++ src/components/CompletedTodos.vue
@@ -1,7 +1,7 @@
 <script setup lang="ts">
-import TodoItem from "./TodoItem.vue";
-import useTodosStore from "@/stores/todos";
-import { ref } from "vue";
+import TodoItem from './TodoItem.vue';
+import useTodosStore from '@/stores/todos';
+import { ref } from 'vue';
 
 const showCompletedTodo = ref(false);
 const store = useTodosStore();
@@ -10,9 +10,11 @@ const store = useTodosStore();
 <template>
   <el-card v-if="store.completedTodos.length > 0" :body-style="{ padding: 'var(--todo-vw)' }" class="selector-card">
     <el-switch v-model="showCompletedTodo" class="switch" />
-    <span> {{ showCompletedTodo ? "Show" : "Hide" }} Completed Todos</span>
+    <span> {{ showCompletedTodo ? 'Show' : 'Hide' }} Completed Todos</span>
   </el-card>
-  <TodoItem v-if="showCompletedTodo" v-for="todo in store.completedTodos" :todo="todo" class="todo-item" />
+  <TransitionGroup name="list">
+    <TodoItem v-if="showCompletedTodo" v-for="todo in store.completedTodos" :todo="todo" class="todo-item" :key="todo.id" />
+  </TransitionGroup>
 </template>
 
 <style scoped>
@@ -30,5 +32,15 @@ const store = useTodosStore();
 }
 .todo-item {
   margin-bottom: 0.5vh;
+}
+
+.list-enter-active,
+.list-leave-active {
+  transition: all 0.5s ease;
+}
+.list-enter-from,
+.list-leave-to {
+  opacity: 0;
+  transform: translateY(-15px);
 }
 </style>
